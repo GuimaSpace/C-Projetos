3726682208238dd68603267ea68c4569c971a416
Vinicius Guimaraes Carneiro <69005421+GuimaSpace@users.noreply.github.com>
2022-09-15 17:00:51 -0300

    Add files via upload

---
+++ CelsiusParaFeK/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+int op = 0;
+Double c, k, f = 0;
+
+
+Console.WriteLine("Informe o valor em celsius");
+c = double.Parse(Console.ReadLine());
+Console.WriteLine("Informe para qual medida você quer converter:");
+Console.WriteLine("1-Kelvin\n2-Fahrenheit");
+op = int.Parse(Console.ReadLine());
+
+if (op == 1)
+{
+    k = c + 273.15;
+    Console.WriteLine("O valor convertido é de: " + k + "");
+    Console.ReadKey();
+}
+else if (op == 2)
+{
+    f = 1.8 * f + 32;
+    Console.WriteLine("O valor convertido é de: " + f + "");
+    Console.ReadKey();
+}
+else 
+{
+    Console.WriteLine("Você informou uma opção não válida.");
+    Console.ReadKey();
+}
